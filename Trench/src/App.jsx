417e167f48295922dae85cc2d3ba0f9be7e372ad
Sheyi Adepoju <sheyiadepoju0@gmail.com>
2026-05-11 15:09:17 -0600

    started trench

---
+++ Trench/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main>
+      <section>
+        <h1>Trench</h1>
+
+        <p>
+          The live mentorship platform for traders.
+        </p>
+
+        <button>
+          Enter The Trenches
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
